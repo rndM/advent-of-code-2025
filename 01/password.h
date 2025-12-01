208444c9de4bec12ee2c149a8bd20be212164123
Renaud MOUREN (R@ndoM) <renaud.mouren@gmail.com>
2025-12-01 21:23:44 +0100

    Add include guards to password.h

--- 01/password.h
+++ 01/password.h
@@ -10,9 +10,14 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PASSWORD_H
+# define PASSWORD_H
+
 #include "ft_printf.h"
 #include "get_next_line.h"
 #include <fcntl.h>
 #include <stdlib.h>
 
 int	ft_atoi(const char *nptr);
+
+#endif // PASSWORD_H
