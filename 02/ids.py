73ca8ce715f3f03142e5e578f21e718f5c36c669
rnd0M <renaud.mouren@gmail.com>
2025-12-02 08:43:26 +0100

    _

---
+++ 02/ids.py
@@ -0,0 +1,16 @@
+def main():
+
+    with open('input.txt', 'r') as f:
+        contenu = f.read().strip()
+
+    print(f"{contenu}")
+
+    ranges = contenu.split(',')
+    print(f"\nb de lignes: {len(ranges)}")
+
+    print("\nDetail des lignes:")
+    for i, r in enumerate(ranges):
+        print(f"  Range {i+1}: '{r}'")
+
+if __name__ == "__main__":
+    main()
