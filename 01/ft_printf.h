b00f383d09308bdbeaf89e667c7e7df0189e6903
rnd0M <renaud.mouren@gmail.com>
2025-12-04 08:52:25 +0100

    _

--- 01/ft_printf.h
+++ 01/ft_printf.h
@@ -4,11 +4,11 @@
 # include <stdarg.h>
 # include <unistd.h>
 
-int			ft_putchar(char c);
-int			ft_putstr(char *str);
-int			ft_putnbr(int n);
-int			ft_putunbr(unsigned int n);
-int			ft_putnbr_base(unsigned long n, char *base);
-int			ft_printf(const char *str, ...);
+int	ft_putchar(char c);
+int	ft_putstr(char *str);
+int	ft_putnbr(int n);
+int	ft_putunbr(unsigned int n);
+int	ft_putnbr_base(unsigned long n, char *base);
+int	ft_printf(const char *str, ...);
 
 #endif // LIBFTPRINF_H
