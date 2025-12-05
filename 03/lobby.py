25f15d26194ab68292bf2e597b25910787dc5788
rmouren <renaud.mouren@gmail.com>
2025-12-05 16:28:32 +0100

    .

--- 03/lobby.py
+++ 03/lobby.py
@@ -1,6 +1,37 @@
 #!/usr/bin/env python3
 
+
+# garder les deux plus grand chiffres de chaques ligne et en faire un nombre
+# additionner tout les nombres pour trouver le joltage
 def main():
+    max_joltage = 0
+
+    with open('input.txt', 'r') as f:
+        file = f.read().split('\n')
+
+    for line in file:
+        joltage = []
+
+        bank = [int(i) for i in line]
+
+        for i in range(2):
+            digit = 0
+            for battery in bank:
+                digit = max(digit, battery)
+            joltage.append(digit)
+            if digit in bank:
+                bank.remove(digit)
+            max_joltage += int(joltage)
+            print(joltage)
+
+    print(max_joltage)
+
+
+if __name__ == "__main__":
+    main()
+
+
+""" def main():
     joltage_output = 0
 
     with open('input.txt', 'r') as f:
@@ -14,8 +45,4 @@ def main():
             max_joltage = max(max_joltage, joltage)
         joltage_output += max_joltage
 
-    print(joltage_output)
-
-
-if __name__ == "__main__":
-    main()
+    print(joltage_output) """
