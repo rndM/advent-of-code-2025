dfe543bcde47218565772e81195027b4e86b7cc4
rnd0M <renaud.mouren@gmail.com>
2025-12-05 20:37:39 +0100

    _

--- 03/lobby.py
+++ 03/lobby.py
@@ -1,48 +1,23 @@
 #!/usr/bin/env python3
 
-
-# garder les deux plus grand chiffres de chaques ligne et en faire un nombre
-# additionner tout les nombres pour trouver le joltage
 def main():
-    max_joltage = 0
+    total_joltage = 0
 
     with open('input.txt', 'r') as f:
-        file = f.read().split('\n')
+        lines = f.read().strip().split('\n')
 
-    for line in file:
-        joltage = []
+    for line in lines:
+        max_joltage = 0
 
-        bank = [int(i) for i in line]
+        for i in range(len(line)):
+            for j in range(i + 1, len(line)):
+                joltage = int(line[i] + line[j])
+                max_joltage = max(max_joltage, joltage)
 
-        for i in range(2):
-            digit = 0
-            for battery in bank:
-                digit = max(digit, battery)
-            joltage.append(digit)
-            if digit in bank:
-                bank.remove(digit)
-            max_joltage += int(joltage)
-            print(joltage)
+        total_joltage += max_joltage
 
-    print(max_joltage)
+    print(f"output joltage: {total_joltage}")
 
 
 if __name__ == "__main__":
     main()
-
-
-""" def main():
-    joltage_output = 0
-
-    with open('input.txt', 'r') as f:
-        file = f.read().split('\n')
-
-    for line in file:
-        max_joltage = 0
-        print(line)
-        for i in range(len(line) - 1):
-            joltage = int(line[i:i+2])
-            max_joltage = max(max_joltage, joltage)
-        joltage_output += max_joltage
-
-    print(joltage_output) """
